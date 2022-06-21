d525a3fb7afd2c7b33cac9cfee8328b676be41be
ismailkhan4 <islamians96@gmail.com>
2022-06-21 16:35:55 +0500

    Redux Thunk functinality implemented

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { postAdded } from './postsSlice';
+import { addNewPost } from './postsSlice';
 import { selectAllUsers } from '../users/usersSlice';
 
 const AddPostForm = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [userId, setUserId] = useState('');
+  const [addRequestStatus, setAddRequestStatus] = useState('idle');
 
   const users = useSelector(selectAllUsers);
 
@@ -17,16 +18,25 @@ const AddPostForm = () => {
 
   const dispatch = useDispatch();
 
-  const onSavePostClicked = () => {
-    if (title && content) {
-      dispatch(postAdded(title, content, userId));
-      setTitle('');
-      setContent('');
-      setUserId('');
-    }
-  };
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === 'idle';
 
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+  const onSavePostClicked = () => {
+    if (canSave) {
+      try {
+        setAddRequestStatus('pending');
+        dispatch(addNewPost({ title, body: content, userId })).unwrap();
+      } catch (err) {
+        console.error('Failed to save the post', err);
+        alert('Failed to save the post', err);
+      } finally {
+        setAddRequestStatus('idle');
+      }
+    }
+    setTitle('');
+    setContent('');
+    setUserId('');
+  };
 
   const usersOptions = users.map((user) => (
     <option key={user.id} value={user.id}>
---
+++ src/features/posts/PostsExcerpt.js
@@ -0,0 +1,21 @@
+import PostAuthor from './PostAuthor';
+import TimeAgo from './TimeAgo';
+import ReactionButtons from './ReactionButtons';
+
+const PostsExcerpt = ({ post }) => {
+  return (
+    <article>
+      <h3>{post.title}</h3>
+      <p>{post.body?.substring(0, 100)}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+      <p>
+        <ReactionButtons post={post} />
+      </p>
+    </article>
+  );
+};
+
+export default PostsExcerpt;
